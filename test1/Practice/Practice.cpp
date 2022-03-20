8d358158b5bc4fd2bdb21e9e3fca9dbd1eb85ca9
WaterDr0ps <3165189679@qq.com>
2022-03-20 23:12:10 +0800

    3.20

---
+++ test1/Practice/Practice.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Dog {
+private:
+	int age;
+	int weight;
+
+public:
+	void setDog(int a, int w) {
+		age = a;
+		weight = w;
+	}
+
+	void printDog() {
+		cout << "age=" << age << endl;
+		cout << "weigth=" << weight << endl;
+	}
+};
+
+int main() {
+	Dog d;
+	d.setDog(20, 50);
+	d.printDog();
+}
