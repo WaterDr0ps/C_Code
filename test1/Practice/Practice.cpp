e8e531b60602efc80e250827285d54ae89bf22a7
WaterDr0ps <3165189679@qq.com>
2022-03-22 23:11:20 +0800

    3.22

--- test1/Practice/Practice.cpp
+++ test1/Practice/Practice.cpp
@@ -1,25 +1,81 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 using namespace std;
+#include <string.h>
 
-class Dog {
+
+class Date {
 private:
-	int age;
-	int weight;
-
+	int year;
+	int month;
+	int day;
 public:
-	void setDog(int a, int w) {
-		age = a;
-		weight = w;
+	Date() {}
+	Date(int y, int m, int d) {
+		year = y;
+		month = m;
+		day = d;
 	}
-
-	void printDog() {
-		cout << "age=" << age << endl;
-		cout << "weigth=" << weight << endl;
+	void set() {
+		cin >> year >> month >> day;
+	}
+	void output()
+	{
+		cout << "  年： " << year << "  月： " << month << "  日： " << day;
 	}
 };
 
+class People {
+private:	
+	int number;
+	char sex;
+	Date birthday;
+	char ID[18];
+public:
+	People(){}
+	People(int n, char s, int y, int m, int d, char id[18]) :birthday(y, m, d) {
+		number = n;
+		sex = s;
+		strcpy(ID, id);
+	}
+	People(People& p) {
+		number = p.number;
+		sex = p.sex;
+		birthday = p.birthday;
+		strcpy(ID, p.ID);
+	}
+	void input(){
+		cout << "录入数据：" << endl;
+		cin >> number >> sex >> ID;
+		birthday.set();
+		
+	}
+	void output()
+	{
+		cout << "学号：" << number << "  性别： " << sex << "  身份证号： " << ID;
+		birthday.output();
+	}
+	~People() {}
+};
+
+
+
 int main() {
-	Dog d;
-	d.setDog(20, 50);
-	d.printDog();
+	Date mybirthday;
+
+	People my;
+
+	my.input();
+
+	People  zhaoxiaojun(my);
+
+	cout << " zhaoxiaojun information is:" << endl;
+
+	cout << "注意性别中m为男，f为女" << endl;
+
+	zhaoxiaojun.output();
+
+	getchar();
+
+	return 0;
 }
